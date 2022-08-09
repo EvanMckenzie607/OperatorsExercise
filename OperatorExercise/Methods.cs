8aa4b9b53c2e3e25d3ec484f18c76c3ef6c871a5
Evan Mckenzie <615evan@gmail.com>
2022-08-09 18:09:02 -0400

    OperatorsExerciseIninitalCommit

---
+++ OperatorExercise/Methods.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorExercise
+{
+    public class Methods
+    {
+        public static double Addition()
+        {
+
+            double a = 17;
+            double b = 4;
+            double ans = a + b;
+            return ans;
+
+
+        }
+
+
+        public static double Sub()
+        {
+
+            double a = 17;
+            double b = 4;
+            double minus = a - b;
+            return minus;
+        }
+
+        public static double Mulx()
+        {
+            double a = 17;
+            double b = 4;
+            double prod = a * b;
+            return prod;
+
+        }
+
+        public static double Divide()
+        {
+            double a = 17;
+            double b = 4;
+            double quo = 17 / 4;
+            return quo;
+
+        }
+
+        public static double Remain()
+        {
+
+            double a = 17;
+            double b = 4;
+            double R = 17 % 4; 
+            return R;
+        }
+
+        public static float AreaOfCircle()
+        {
+            
+            double radius = Double.Parse(Console.ReadLine());
+            radius = Math.PI * Math.Pow(radius, 2);
+            return (float)radius;
+
+
+
+
+
+        }
+
+    }
+
+
+}
+
+
+
+
---
+++ OperatorExercise/Program.cs
@@ -0,0 +1,24 @@
+﻿//Operators Exersice #1
+using OperatorExercise;
+
+var ans = Methods.Addition();
+var minus = Methods.Sub();
+var prod = Methods.Mulx();
+var quo = Methods.Divide();
+var remain = Methods.Remain();
+
+Console.WriteLine($"17 + 4 = {ans}");
+Console.WriteLine($"17 - 4 = {minus}");
+Console.WriteLine($"17 * 4 = {prod} \n17 / 4 = {quo} with a remainder of {remain} ");
+//Begin Exercise #2
+Console.WriteLine("I see you have a circle \nWhat is the Radius of your circle? ");
+
+var radius = Methods.AreaOfCircle(); 
+
+Console.WriteLine($"The area of your circle is {radius}");
+
+
+
+
+
+
